d7221b9eadae972a75fb69f0a593cdc1c7e3acf5
ArielDW <68842619+ArielDW@users.noreply.github.com>
2023-01-07 22:05:03 -0600

    fixed formatting

--- monitor.py
+++ monitor.py
@@ -7,18 +7,20 @@ import time
 def get_cpu_load():
     cpu_load = open("/proc/loadavg", "r")
     load = cpu_load.read().split()[:3]
-    print(f"CPU; 1 min: {load[0]}%, 5 mins: {load[1]}%, 15 mins: {load[2]}%")
+    print(
+        f"CPU; 1 min avg: {load[0]}%, 5 mins avg: {load[1]}%, 15 mins avg: {load[2]}%")
     cpu_load.close()
 
 
 def get_memory_usage():
     memory = open("/proc/meminfo", "r")
     rows = memory.readlines()
-    total = int(rows[0].split()[1])
-    free = int(rows[1].split()[1])
+    total = round(int(rows[0].split()[1])/1024, 2)
+    free = round(int(rows[1].split()[1])/1024, 2)
     used = total - free
-    percentage = used / total * 100
-    print(f"RAM; total {total}, free {free}, used {used} ({percentage}%)")
+    percentage = round(used/total*100, 2)
+    print(
+        f"RAM; total {total} MB, free {free} MB, used {used} MB ({percentage}%)")
     memory.close()
 
 
