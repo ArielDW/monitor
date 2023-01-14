3f72d6171a802091931eea73390e1f40dd4bb746
ArielDW <68842619+ArielDW@users.noreply.github.com>
2023-01-13 21:32:51 -0600

    added run.py with menu skeleton

---
+++ run.py
@@ -0,0 +1,35 @@
+from sys_info import get_systen_load
+
+
+def menu():
+    print("Select an option:")
+    print("1. System information")
+    print("2. Alert settings")
+    print("3. Start daemon")
+    print("4. Stop daemon")
+    print("5. Exit")
+
+    choice = int(input("> "))
+
+    if choice == 1:
+        get_systen_load()
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    elif choice == 4:
+        pass
+    elif choice == 5:
+        pass
+
+    else:
+        print("Invalid choice")
+        menu()
+
+
+def main():
+    menu()
+
+
+if __name__ == "__main__":
+    main()
