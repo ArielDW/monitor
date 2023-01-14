dbf27986e28522feeeaed3b421b02e7265a8f20c
ArielDW <68842619+ArielDW@users.noreply.github.com>
2023-01-13 19:50:38 -0600

    unified information

--- monitor.py
+++ monitor.py
@@ -2,30 +2,40 @@ import os
 import time
 
 
-def get_cpu_load():
+def get_systen_load():
+    print("System information")
+    system_info = open("/etc/os-release", "r")
+    sysinfo = system_info.readlines()
+    print(f"OS: {sysinfo[6].split('=')[1]}")
+    system_info.close()
+    print("\n")
+    hostname = open("/etc/hostname", "r")  # fix whitespace
+    hn = hostname.read().split()[0]
+    print(f"Hostname: {hn}")
+    hostname.close()
+    print("\n")
     cpu_load = open("/proc/loadavg", "r")
     load = (cpu_load.read().split()[:3]) * 100
+    print("CPU load")
     print(
-        f"CPU; 1 min avg: {100*float(load[0])}%, 5 mins avg: {100*float(load[1])}%, 15 mins avg: {100*float(load[2])}%")
+        f"1 min avg: {100*float(load[0])}%, 5 mins avg: {100*float(load[1])}%, 15 mins avg: {100*float(load[2])}%")
     cpu_load.close()
-
-
-def get_memory_usage():
+    print("\n")
     memory = open("/proc/meminfo", "r")
     rows = memory.readlines()
     total = round(int(rows[0].split()[1])/1024, 2)
     free = round(int(rows[1].split()[1])/1024, 2)
     used = total - free
     percentage = round(used/total*100, 2)
+    print("Memory usage")
     print(
-        f"RAM; total {total} MB, free {free} MB, used {used} MB ({percentage}%)")
+        f"total {total} MB, free {free} MB, used {round(used,2)} MB ({percentage}%)")
     memory.close()
 
 
-def get_storage_utilization():
-    storage_utilization = os.system('')
+def main():
+    get_systen_load()
 
 
-get_cpu_load()
-get_memory_usage()
-get_storage_utilization()
+if __name__ == "__main__":
+    main()
