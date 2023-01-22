9d0dd6753dc17515afbad56e809e384aa3d5cc88
ArielDW <68842619+ArielDW@users.noreply.github.com>
2023-01-22 08:55:43 -0600

    added basic timestamp log functionality

--- log.py
+++ log.py
@@ -1,2 +1,13 @@
 import datetime
+import os
 
+if not os.path.exists("system_records.log"):
+    with open("system_records.log", "w") as log_file:
+        timestamp = str(datetime.datetime.now())
+        log_file.write(timestamp + "\n")
+        print("New file created")
+else:
+    with open("system_records.log", "a") as log_file:
+        timestamp = str(datetime.datetime.now())
+        log_file.write(timestamp + "\n")
+        print("OK")
