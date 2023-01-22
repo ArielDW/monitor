7269b3fe1e89ca358ba8c274413243298f67a688
ArielDW <68842619+ArielDW@users.noreply.github.com>
2023-01-22 08:42:00 -0600

    merged info.py functionality into run.py

--- run.py
+++ run.py
@@ -1,12 +1,34 @@
 import os
+import platform
+import psutil
+
+
+def get_system_info():
+    os.system("clear")
+    print("System information")
+    print(f"OS: {platform.system()} {platform.release()}")
+    print(f"Hostname: {platform.node()}")
+
+    cpu_load = psutil.getloadavg()
+    print("CPU load average")
+    print(
+        f"Past 1 min: {round(cpu_load[0],2)}%, 5 mins: {round(cpu_load[1],2)}%, 15 mins: {round(cpu_load[2],2)}%")
+
+    memory = psutil.virtual_memory()
+    total = round(memory.total/1024**2, 2)
+    free = round(memory.free/1024**2, 2)
+    used = total - free
+    percentage = round(used/total*100, 2)
+    print("Memory usage")
+    print(
+        f"total {total} MB, free {free} MB, used {round(used,2)} MB ({percentage}%)")
 
 
 def menu():
     os.system("clear")
-    from info import get_system_info
     get_system_info()
     print("\n")
-    print("Select an option:")
+    print("Choose an option:")
     print("1. Refresh system information")
     print("2. Alert settings")
     print("3. Start daemon")
