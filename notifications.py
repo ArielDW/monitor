c0210b256c254ea0cf69f9622655b647680f77a1
ArielDW <68842619+ArielDW@users.noreply.github.com>
2023-01-22 11:31:54 -0600

    added exception handling for delivery error

--- notifications.py
+++ notifications.py
@@ -2,16 +2,26 @@ import os
 import json
 from twilio.rest import Client
 
-with open("settings.json", "r") as f:
-    settings = json.load(f)
-    account_sid = f"{settings['CPU']}"
-    auth_token = ""
-    client = Client(account_sid, auth_token)
+# retreives information from json file
+try:
+    with open("settings.json", "r") as f:
+        settings = json.load(f)
+        account_sid = f"{settings['twilio_account_sid']}"
+        auth_token = f"{settings['twilio_auth_token']}"
 
-message = client.messages.create(
-    body="Hello from Twilio",
-    from_="+16089339250",
-    to="+525520270281"
-)
+        # Pending...
+        '''
+        client = Client(account_sid, auth_token)
 
-print(message.sid)
+        message = client.messages.create(
+            body="Hello from Twilio",
+            from_="+16089339250",
+            to="+525520270281"
+        )
+
+    print(message.sid)
+    '''
+except IOError as error:
+    import sys
+    print("Error: Notification failed", error, file=sys.stderr)
+    sys.exit(4)
