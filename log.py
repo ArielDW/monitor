65d28597bbc85c2f7e54eef6143f7ca681402ab1
ArielDW <68842619+ArielDW@users.noreply.github.com>
2023-01-22 09:21:15 -0600

    modified log.py sctruc for readability

--- log.py
+++ log.py
@@ -1,13 +1,17 @@
 import datetime
 import os
 
-if not os.path.exists("system_records.log"):
-    with open("system_records.log", "w") as log_file:
-        timestamp = str(datetime.datetime.now())
-        log_file.write(timestamp + "\n")
-        print("New file created")
-else:
+
+def add_entry():
     with open("system_records.log", "a") as log_file:
         timestamp = str(datetime.datetime.now())
         log_file.write(timestamp + "\n")
         print("OK")
+
+
+if not os.path.exists("system_records.log"):
+    with open("system_records.log", "w") as log_file:
+        add_entry()
+else:
+    with open("system_records.log", "a") as log_file:
+        add_entry()
