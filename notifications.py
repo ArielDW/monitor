c9e0d6b0bbd3d17e1ef8546c455f426df9428a11
ArielDW <68842619+ArielDW@users.noreply.github.com>
2023-01-22 09:37:09 -0600

    added option to write twilio settings to json file

--- notifications.py
+++ notifications.py
@@ -1,8 +1,6 @@
 import os
 from twilio.rest import Client
 
-# Set environment variables for your credentials
-# Read more at http://twil.io/secure
 account_sid = ""
 auth_token = ""
 client = Client(account_sid, auth_token)
