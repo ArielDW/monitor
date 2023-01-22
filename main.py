c16788d68562c65b286993da1c9316f21d3d49f1
ArielDW <68842619+ArielDW@users.noreply.github.com>
2023-01-22 16:04:39 -0600

    add menu options for background process

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
 import platform
 import psutil
+import json
+import time
+
+os.system("clear")
 
 
 def get_system_info():
@@ -12,7 +16,7 @@ def get_system_info():
     cpu_load = psutil.getloadavg()
     print("CPU load average")
     print(
-        f"Past 1 min: {round(cpu_load[0],2)}%, 5 mins: {round(cpu_load[1],2)}%, 15 mins: {round(cpu_load[2],2)}%")
+        f"Past 1 min: {round(cpu_load[0]*100,2)}%, 5 mins: {round(cpu_load[1]*100,2)}%, 15 mins: {round(cpu_load[2]*100,2)}%")
 
     memory = psutil.virtual_memory()
     total = round(memory.total/1024**2, 2)
@@ -21,7 +25,7 @@ def get_system_info():
     percentage = round(used/total*100, 2)
     print("Memory usage")
     print(
-        f"total {total} MB, free {free} MB, used {round(used,2)} MB ({percentage}%)")
+        f"Total {total} MB, free {free} MB, used {round(used,2)} MB ({percentage}%)")
 
 
 def menu():
@@ -31,8 +35,8 @@ def menu():
     print("Choose an option:")
     print("1. Refresh system information")
     print("2. Alert settings")
-    print("3. Start daemon")
-    print("4. Stop daemon")
+    print("3. Start background process")
+    print("4. Stop background process")
     print("5. Exit")
 
     choice = int(input("> "))
@@ -44,9 +48,37 @@ def menu():
         from settings import update_settings
         update_settings()
     elif choice == 3:
-        pass
+        if not os.path.exists("settings.json"):
+            os.system("clear")
+            print("Configuration file error, please update settings.")
+            time.sleep(2)
+            from settings import update_settings
+            update_settings()
+        else:
+            with open("settings.json", "r") as f:
+                settings = json.load(f)
+                os.system("clear")
+                settings["keep_alive"] = "True"
+                with open("settings.json", "w") as f:
+                    json.dump(settings, f)
+                menu()
+
     elif choice == 4:
-        pass
+        if not os.path.exists("settings.json"):
+            os.system("clear")
+            print("Configuration file error, please update settings.")
+            time.sleep(2)
+            from settings import update_settings
+            update_settings()
+        else:
+            with open("settings.json", "r") as f:
+                settings = json.load(f)
+                os.system("clear")
+                settings["keep_alive"] = "False"
+                with open("settings.json", "w") as f:
+                    json.dump(settings, f)
+                menu()
+
     elif choice == 5:
         exit()
 
